9d2ac218fb5e5dc23cf95ee85d9ffa328bb2d67d
CodingCreature <CodingCreate101@users.noreply.github.com>
2017-10-12 16:57:03 +0530

    Update structure.php

--- structure.php
+++ structure.php
@@ -1,8 +1,8 @@
 <html>
-<title>BG Chapter <Chapter number></title>
+<title>BG Chapter "Chapter number"</title>
 
 <body>
-<h1>Chapter <Chapter number></h1>
+<h1>Chapter "Chapter number"</h1>
 
 -- verses --
   
