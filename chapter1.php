13cdcc9ee0b91ec6e2d762d2c9d1b6ef045200f5
CodingCreature <CodingCreate101@users.noreply.github.com>
2017-10-12 16:08:07 +0530

    Add files via upload

---
+++ chapter1.php
@@ -0,0 +1,49 @@
+<html>
+<title></title>
+
+<body>
+    
+
+
+
+<h1>Chapter 1</h1>
+
+verse-1
+धृतराष्ट्र उवाच |
+धर्मक्षेत्रे कुरुक्षेत्रे समवेता युयुत्सवः |
+मामकाः पाण्डवाश्चैव किमकुर्वत सञ्जय ||1||
+
+verse-1
+dhṛitarāśhtra uvācha
+dharma-kṣhetre kuru-kṣhetre samavetā yuyutsavaḥ
+māmakāḥ pāṇḍavāśhchaiva kimakurvata sañjaya
+
+verse-1
+dhṛitarāśhtraḥ uvācha—Dhritarashtra said; dharma-kṣhetre—the land of dharma; kuru-kṣhetre—at Kurukshetra; samavetāḥ—having gathered; yuyutsavaḥ—desiring to fight; māmakāḥ—my sons; pāṇḍavāḥ—the sons of Pandu; cha—and; eva—certainly; kim—what; akurvata—did they do; sañjaya—Sanjay
+
+verse-1
+BG 1.1: Dhritarashtra said: O Sanjay, after gathering on the holy field of Kurukshetra, and desiring to fight, what did my sons and the sons of Pandu do?
+verse-1
+
+verse-2
+सञ्जय उवाच ।
+दृष्ट्वा तु पाण्डवानीकं व्यूढं दुर्योधनस्तदा ।
+आचार्यमुपसङ्गम्य राजा वचनमब्रवीत् ।। 2।।
+
+verse-2
+sañjaya uvācha
+dṛiṣhṭvā tu pāṇḍavānīkaṁ vyūḍhaṁ duryodhanastadā
+āchāryamupasaṅgamya rājā vachanamabravīt
+
+verse-2
+sanjayaḥ uvācha—Sanjay said; dṛiṣhṭvā—on observing; tu—but; pāṇḍava-anīkam—the Pandava army; vyūḍham—standing in a military formation; duryodhanaḥ—King Duryodhan; tadā—then; āchāryam—teacher; upasaṅgamya—approached; rājā—the king; vachanam—words; abravīt—spoke
+
+verse-2
+BG 1.2: Sanjay said: On observing the Pandava army standing in military formation, King Duryodhan approached his teacher Dronacharya, and said the following words.
+verse-2
+
+
+
+</body>
+
+</html>
