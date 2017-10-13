64dc7e395ce817061e3b7f057e2dd472e3c5b617
CodingCreature <CodingCreate101@users.noreply.github.com>
2017-10-14 00:26:18 +0530

    Chapter 12 completed. Open for review.

---
+++ chapter12.php
@@ -0,0 +1,254 @@
+<html>
+<title>BG Chapter 12</title>
+<body>
+    
+
+<h1>Chapter 12: Bhakti Yoga</h1>
+
+verse-1
+अर्जुन उवाच |
+एवं सततयुक्ता ये भक्तास्त्वां पर्युपासते |
+ये चाप्यक्षरमव्यक्तं तेषां के योगवित्तमा: || 1||
+verse-1
+arjuna uvācha
+evaṁ satata-yuktā ye bhaktās tvāṁ paryupāsate
+ye chāpy akṣharam avyaktaṁ teṣhāṁ ke yoga-vittamāḥ
+verse-1
+arjunaḥ uvācha—Arjun said; evam—thus; satata—steadfastly; yuktāḥ—devoted; ye—those; bhaktāḥ—devotees; tvām—you; paryupāsate—worship; ye—those; cha—and; api—also; akṣharam—the imperishable; avyaktam—the formless Brahman; teṣhām—of them; ke—who; yoga-vit-tamāḥ—more perfect in Yog
+verse-1
+BG 12.1: Arjun inquired: Between those who are steadfastly devoted to your personal form and those who worship the formless Brahman, who do you consider to be more perfect in Yog?
+verse-1
+
+verse-2
+श्रीभगवानुवाच |
+मय्यावेश्य मनो ये मां नित्ययुक्ता उपासते |
+श्रद्धया परयोपेतास्ते मे युक्ततमा मता: || 2||
+verse-2
+śhrī-bhagavān uvācha
+mayy āveśhya mano ye māṁ nitya-yuktā upāsate
+śhraddhayā parayopetās te me yuktatamā matāḥ
+verse-2
+śhrī-bhagavān uvācha—the Blessed Lord said; mayi—on me; āveśhya—fix; manaḥ—the mind; ye—those; mām—me; nitya yuktāḥ—always engaged; upāsate—worship; śhraddhayā—with faith; parayā—best; upetāḥ—endowed; te—they; me—by me; yukta-tamāḥ—situated highest in Yog; matāḥ—I consider
+verse-2
+BG 12.2: The Blessed Lord said: Those who fix their minds on me and always engage in my devotion with steadfast faith, I consider them to be the best yogis.
+verse-2
+
+verse-3
+ये त्वक्षरमनिर्देश्यमव्यक्तं पर्युपासते |
+सर्वत्रगमचिन्त्यञ्च कूटस्थमचलन्ध्रुवम् || 3||
+verse-3
+ye tv akṣharam anirdeśhyam avyaktaṁ paryupāsate
+sarvatra-gam achintyañcha kūṭa-stham achalandhruvam
+verse-3
+ye—who; tu—but; akṣharam—the imperishable; anirdeśhyam—the indefinable; avyaktam—the unmanifest; paryupāsate—worship; sarvatra-gam—the all-pervading; achintyam—the unthinkable; cha—and; kūṭa-stham—the unchanging; achalam—the immovable; dhruvam—the eternal; 
+verse-3
+BG 12.3: But those who worship the formless aspect of the Absolute Truth—the imperishable, the indefinable, the unmanifest, the all-pervading, the unthinkable, the unchanging, the eternal, and the immoveable—
+verse-3
+
+verse-4
+सन्नियम्येन्द्रियग्रामं सर्वत्र समबुद्धय: |
+ते प्राप्नुवन्ति मामेव सर्वभूतहिते रता: || 4||
+verse-4
+sanniyamyendriya-grāmaṁ sarvatra sama-buddhayaḥ
+te prāpnuvanti mām eva sarva-bhūta-hite ratāḥ
+verse-4
+sanniyamya—restraining; indriya-grāmam—the senses; sarvatra—everywhere; sama-buddhayaḥ—even-minded; te—they; prāpnuvanti—attain; mām—me; eva—also; sarva-bhūta-hite—in the welfare of all beings; ratāḥ—engaged
+verse-4
+BG 12.4: ..by restraining their senses and being even-minded everywhere, such persons, engaged in the welfare of all beings, also attain me.
+verse-4
+
+verse-5
+क्लेशोऽधिकतरस्तेषामव्यक्तासक्तचेतसाम् ||
+अव्यक्ता हि गतिर्दु:खं देहवद्भिरवाप्यते || 5||
+verse-5
+kleśho ’dhikataras teṣhām avyaktāsakta-chetasām
+avyaktā hi gatir duḥkhaṁ dehavadbhir avāpyate
+verse-5
+kleśhaḥ—tribulations; adhika-taraḥ—full of; teṣhām—of those; avyakta—to the unmanifest; āsakta—attached; chetasām—whose minds; avyaktā—the unmanifest; hi—indeed; gatiḥ—path; duḥkham—exceeding difficulty; deha-vadbhiḥ—for the embodied; avāpyate—is reached
+verse-5
+BG 12.5: For those whose minds are attached to the unmanifest, the path of realization is full of tribulations. Worship of the unmanifest is exceedingly difficult for embodied beings.
+verse-5
+
+verse-6
+ये तु सर्वाणि कर्माणि मयि संन्न्यस्य मत्पर: |
+अनन्येनैव योगेन मां ध्यायन्त उपासते || 6||
+verse-6
+ye tu sarvāṇi karmāṇi mayi sannyasya mat-paraḥ
+ananyenaiva yogena māṁ dhyāyanta upāsate
+verse-6
+ye—who; tu—but; sarvāṇi—all; karmāṇi—actions; mayi—to me; sannyasya—dedicating; mat-paraḥ—regarding me as the Supreme goal; ananyena—exclusively; eva—certainly; yogena—with devotion; mām—me; dhyāyantaḥ—meditating; upāsate—worship;
+verse-6
+BG 12.6: But those who dedicate all their actions to me, regarding me as the Supreme goal, worshiping me and meditating on me with exclusive devotion,
+verse-6
+
+verse-7
+तेषामहं समुद्धर्ता मृत्युसंसारसागरात् |
+भवामि नचिरात्पार्थ मय्यावेशितचेतसाम् || 7||
+verse-7
+teṣhām ahaṁ samuddhartā mṛityu-saṁsāra-sāgarāt
+bhavāmi na chirāt pārtha mayy āveśhita-chetasām
+verse-7
+teṣhām—of those; aham—I; samuddhartā—the deliverer; mṛityu-saṁsāra-sāgarāt—from the ocean of birth and death; bhavāmi—(I) become; na—not; chirāt—after a long time; pārtha—Arjun, the son of Pritha; mayi—with me; āveśhita chetasām—of those whose consciousness is united
+verse-7
+BG 12.7: ..O Parth, I swiftly deliver them from the ocean of birth and death, for their consciousness is united with me.
+verse-7
+
+verse-8
+मय्येव मन आधत्स्व मयि बुद्धिं निवेशय |
+निवसिष्यसि मय्येव अत ऊर्ध्वं न संशय: || 8||
+verse-8
+mayy eva mana ādhatsva mayi buddhiṁ niveśhaya
+nivasiṣhyasi mayy eva ata ūrdhvaṁ na sanśhayaḥ
+verse-8
+mayi—on me; eva—alone; manaḥ—mind; ādhatsva—fix; mayi—on me; buddhim—intellect; niveśhaya—surrender; nivasiṣhyasi—you shall always live; mayi—in me; eva—alone; ataḥ ūrdhvam—thereafter; na—not; sanśhayaḥ—doubt
+verse-8
+BG 12.8: Fix your mind on me alone and surrender your intellect to me. There upon, you will always live in me. Of this, there is no doubt.
+verse-8
+
+verse-9
+अथ चित्तं समाधातुं न शक्नोषि मयि स्थिरम् |
+अभ्यासयोगेन ततो मामिच्छाप्तुं धनञ्जय || 9||
+verse-9
+atha chittaṁ samādhātuṁ na śhaknoṣhi mayi sthiram
+abhyāsa-yogena tato mām ichchhāptuṁ dhanañjaya
+verse-9
+atha—if; chittam—mind; samādhātum—to fix; na śhaknoṣhi—(you) are unable; mayi—on me; sthiram—steadily; abhyāsa-yogena—by uniting with God through repeated practice; tataḥ—then; mām—me; ichchhā—desire; āptum—to attain; dhanañjaya—Arjun, the conqueror of wealth
+verse-9
+BG 12.9: If you are unable to fix your mind steadily on me, O Arjun, then practice remembering me with devotion while constantly restraining the mind from worldly affairs.
+verse-9
+
+verse-10
+अभ्यासेऽप्यसमर्थोऽसि मत्कर्मपरमो भव |
+मदर्थमपि कर्माणि कुर्वन्सिद्धिमवाप्स्यसि || 10||
+verse-10
+abhyāse ’py asamartho ’si mat-karma-paramo bhava
+mad-artham api karmāṇi kurvan siddhim avāpsyasi
+verse-10
+abhyāse—in practice; api—if; asamarthaḥ—unable; asi—you; mat-karma paramaḥ—devotedly work for me; bhava—be; mat-artham—for my sake; api—also; karmāṇi—work; kurvan—performing; siddhim—perfection; avāpsyasi—you shall achieve
+verse-10
+BG 12.10: If you cannot practice remembering me with devotion, then just try to work for me. Thus performing devotional service to me, you shall achieve the stage of perfection.
+verse-10
+
+verse-11
+अथैतदप्यशक्तोऽसि कर्तुं मद्योगमाश्रित: |
+सर्वकर्मफलत्यागं तत: कुरु यतात्मवान् || 11||
+verse-11
+athaitad apy aśhakto ’si kartuṁ mad-yogam āśhritaḥ
+sarva-karma-phala-tyāgaṁ tataḥ kuru yatātmavān
+verse-11
+atha—if; etat—this; api—even; aśhaktaḥ—unable; asi—you are; kartum—to work; mad-yogam—with devotion to me; āśhritaḥ—taking refuge; sarva-karma—of all actions; phala-tyāgam—to renounce the fruits; tataḥ—then; kuru—do; yata-ātma-vān—be situated in the self
+verse-11
+BG 12.11: If you are unable to even work for me in devotion, then try to renounce the fruits of your actions and be situated in the self.
+verse-11
+
+verse-12
+श्रेयो हि ज्ञानमभ्यासाज्ज्ञानाद्ध्यानं विशिष्यते |
+ध्यानात्कर्मफलत्यागस्त्यागाच्छान्तिरनन्तरम् || 12||
+verse-12
+śhreyo hi jñānam abhyāsāj jñānād dhyānaṁ viśhiṣhyate
+dhyānāt karma-phala-tyāgas tyāgāch chhāntir anantaram
+verse-12
+śhreyaḥ—better; hi—for; jñānam—knowledge; abhyāsāt—than (mechanical) practice; jñānāt—than knowledge; dhyānam—meditation; viśhiṣhyate—better; dhyānāt—than meditation; karma-phala-tyāgaḥ—renunciation of the fruits of actions; tyāgāt—renunciation; śhāntiḥ—peace; anantaram—immediately
+verse-12
+BG 12.12: Better than mechanical practice is knowledge; better than knowledge is meditation. Better than meditation is renunciation of the fruits of actions, for peace immediately follows such renunciation.
+verse-12
+
+verse-13
+अद्वेष्टा सर्वभूतानां मैत्र: करुण एव च |
+निर्ममो निरहङ्कार: समदु:खसुख: क्षमी || 13||
+verse-13
+adveṣhṭā sarva-bhūtānāṁ maitraḥ karuṇa eva cha
+nirmamo nirahankāraḥ sama-duḥkha-sukhaḥ kṣhamī
+verse-13
+adveṣhṭā—free from malice; sarva-bhūtānām—toward all living beings; maitraḥ—friendly; karuṇaḥ—compassionate; eva—indeed; cha—and; nirmamaḥ—free from attachment to possession; nirahankāraḥ—free from egoism; sama—equipoised; duḥkha—distress; sukhaḥ—happiness; kṣhamī—forgiving;
+verse-13
+BG 12.13: Those devotees are very dear to me who are free from malice toward all living beings, who are friendly, and compassionate. They are free from attachment to possessions and egotism, equipoised in happiness and distress, and ever-forgiving.
+verse-13
+
+verse-14
+सन्तुष्ट: सततं योगी यतात्मा दृढनिश्चय: |
+मय्यर्पितमनोबुद्धिर्यो मद्भक्त: स मे प्रिय: || 14||
+verse-14
+santuṣhṭaḥ satataṁ yogī yatātmā dṛiḍha-niśhchayaḥ
+mayy arpita-mano-buddhir yo mad-bhaktaḥ sa me priyaḥ
+verse-14
+santuṣhṭaḥ—contented; satatam—steadily; yogī—united in devotion; yata-ātmā—self-controlled; dṛiḍha-niśhchayaḥ—firm in conviction; mayi—to me; arpita—dedicated; manaḥ—mind; buddhiḥ—intellect; yaḥ—who; mat-bhaktaḥ—my devotees; saḥ—they; me—to me; priyaḥ—very dear
+verse-14
+BG 12.14: They are ever-contented, steadily united with me in devotion, self-controlled, firm in conviction, and dedicated to me in mind and intellect.
+verse-14
+
+verse-15
+यस्मान्नोद्विजते लोको लोकान्नोद्विजते च य: |
+हर्षामर्षभयोद्वेगैर्मुक्तो य: स च मे प्रिय: || 15||
+verse-15
+yasmān nodvijate loko lokān nodvijate cha yaḥ
+harṣhāmarṣha-bhayodvegair mukto yaḥ sa cha me priyaḥ
+verse-15
+yasmāt—by whom; na—not; udvijate—are agitated; lokaḥ—people; lokāt—from people; na—not; udvijate—are disturbed; cha—and; yaḥ—who; harṣha—pleasure; amarṣha—pain; bhaya—fear; udvegaiḥ—anxiety; muktaḥ—freed; yaḥ—who; saḥ—they; cha—and; me—to me; priyaḥ—very dear
+verse-15
+BG 12.15: Those who are not a source of annoyance to anyone and who in turn are not agitated by anyone, who are equal in pleasure and pain, and free from fear and anxiety, such devotees of mine are very dear to me.
+verse-15
+
+verse-16
+अनपेक्ष: शुचिर्दक्ष उदासीनो गतव्यथ: |
+सर्वारम्भपरित्यागी यो मद्भक्त: स मे प्रिय: || 16||
+verse-16
+anapekṣhaḥ śhuchir dakṣha udāsīno gata-vyathaḥ
+sarvārambha-parityāgī yo mad-bhaktaḥ sa me priyaḥ
+verse-16
+anapekṣhaḥ—indifferent to worldly gain; śhuchiḥ—pure; dakṣhaḥ—skillful; udāsīnaḥ—without cares; gata-vyathaḥ—untroubled; sarva-ārambha—of all undertakings; parityāgī—renouncer; saḥ—who; mat-bhaktaḥ—my devotee; saḥ—he; me—to ne; priyaḥ—very dear
+verse-16
+BG 12.16: Those who are indifferent to worldly gain, externally and internally pure, skillful, without cares, untroubled, and free from selfishness in all undertakings, such devotees of mine are very dear to me.
+verse-16
+
+verse-17
+यो न हृष्यति न द्वेष्टि न शोचति न काङ् क्षति |
+शुभाशुभपरित्यागी भक्तिमान्य: स मे प्रिय: || 17||
+verse-17
+yo na hṛiṣhyati na dveṣhṭi na śhochati na kāṅkṣhati
+śhubhāśhubha-parityāgī bhaktimān yaḥ sa me priyaḥ
+verse-17
+yaḥ—who; na—neither; hṛiṣhyati—rejoice; na—nor; dveṣhṭi—despair; na—neither; śhochati—lament; na—nor; kāṅkṣhati—hanker for gain; śhubha-aśhubha-parityāgī—who renounce both good and evil deeds; bhakti-mān—full of devotion; yaḥ—who; saḥ—that person; me—to me; priyaḥ—very dear
+verse-17
+BG 12.17: Those who neither rejoice in mundane pleasures nor despair in worldly sorrows, who neither lament for any loss nor hanker for any gain, who renounce both good and evil deeds, such persons who are full of devotion are very dear to me.
+verse-17
+
+verse-18
+सम: शत्रौ च मित्रे च तथा मानापमानयो: |
+शीतोष्णसुखदु:खेषु सम: सङ्गविवर्जित: || 18||
+verse-18
+samaḥ śhatrau cha mitre cha tathā mānāpamānayoḥ
+śhītoṣhṇa-sukha-duḥkheṣhu samaḥ saṅga-vivarjitaḥ
+verse-18
+samaḥ—alike; śhatrau—to a foe; cha—and; mitre—to a friend; cha tathā—as well as; māna-apamānayoḥ—in honor and dishonor; śhīta-uṣhṇa—in cold and heat; sukha-duḥkheṣhu—in joy and sorrow; samaḥ—equipoised; saṅga-vivarjitaḥ—free from all unfavorable association;
+verse-18
+BG 12.18: Those, who are alike to friend and foe, equipoised in honor and dishonor, cold and heat, joy and sorrow, and are free from all unfavorable association;
+verse-18
+
+verse-19
+तुल्यनिन्दास्तुतिर्मौनी सन्तुष्टो येन केनचित् |
+अनिकेत: स्थिरमतिर्भक्तिमान्मे प्रियो नर: || 19||
+verse-19
+tulya-nindā-stutir maunī santuṣhṭo yena kenachit
+aniketaḥ sthira-matir bhaktimān me priyo naraḥ
+verse-19
+tulya—alike; nindā-stutiḥ—reproach and praise; maunī—silent contemplation; santuṣhṭaḥ—contented; yena kenachit—with anything; aniketaḥ—without attachment to the place of residence; sthira—firmly fixed; matiḥ—intellect; bhakti-mān—full of devotion; me—to me; priyaḥ—very dear; naraḥ—a person
+verse-19
+BG 12.19: those who take praise and reproach alike, who are given to silent contemplation, content with what comes their way, without attachment to the place of residence, whose intellect is firmly fixed in me, and who are full of devotion to me, such persons are very dear to me.
+verse-19
+
+verse-20
+ये तु धर्म्यामृतमिदं यथोक्तं पर्युपासते |
+श्रद्दधाना मत्परमा भक्तास्तेऽतीव मे प्रिया: || 20||
+verse-20
+ye tu dharmyāmṛitam idaṁ yathoktaṁ paryupāsate
+śhraddadhānā mat-paramā bhaktās te ’tīva me priyāḥ
+verse-20
+ye—who; tu—indeed; dharma—of wisdom; amṛitam—nectar; idam—this; yathā—as; uktam—declared; paryupāsate—exclusive devotion; śhraddadhānāḥ—with faith; mat-paramāḥ—intent on me as the supreme goal; bhaktāḥ—devotees; te—they; atīva—exceedingly; me—to me; priyāḥ—dear
+verse-20
+BG 12.20: Those who honor this nectar of wisdom declared here, have faith in me, and are devoted and intent on me as the supreme goal, they are exceedingly dear to me.
+verse-20
+
+
+</body>
+</html>
