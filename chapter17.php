2df9da9be7a0565d483c5949b322d907fadbe19a
CodingCreature <CodingCreate101@users.noreply.github.com>
2017-10-14 00:57:09 +0530

    Chapter 17 completed. Open for review.

---
+++ chapter17.php
@@ -0,0 +1,349 @@
+<html>
+<title>BG Chapter 17</title>
+<body>
+    
+
+<h1>Chapter 17: Śhraddhā Traya Vibhāga Yoga</h1>
+
+verse-1
+अर्जुन उवाच |
+ये शास्त्रविधिमुत्सृज्य यजन्ते श्रद्धयान्विता: |
+तेषां निष्ठा तु का कृष्ण सत्वमाहो रजस्तम: || 1||
+verse-1
+arjuna uvācha
+ye śhāstra-vidhim utsṛijya yajante śhraddhayānvitāḥ
+teṣhāṁ niṣhṭhā tu kā kṛiṣhṇa sattvam āho rajas tamaḥ
+verse-1
+arjunaḥ uvācha—Arjun said; ye—who; śhāstra-vidhim—scriptural injunctions; utsṛijya—disregard; yajante—worship; śhraddhayā-anvitāḥ—with faith; teṣhām—their; niṣhṭhā—faith; tu—indeed; kā—what; kṛiṣhṇa—Krishna; sattvam—mode of goodness; āho—or; rajaḥ—mode of passion; tamaḥ—mode of ignorance
+verse-1
+BG 17.1: Arjun said: O Krishna, where do they stand who disregard the injunctions of the scriptures, but still worship with faith? Is their faith in the mode of goodness, passion, or ignorance?
+verse-1
+
+verse-2
+श्रीभगवानुवाच |
+त्रिविधा भवति श्रद्धा देहिनां सा स्वभावजा |
+सात्विकी राजसी चैव तामसी चेति तां शृणु || 2||
+verse-2
+śhrī-bhagavān uvācha
+tri-vidhā bhavati śhraddhā dehināṁ sā svabhāva-jā
+sāttvikī rājasī chaiva tāmasī cheti tāṁ śhṛiṇu
+verse-2
+śhrī-bhagavān uvācha—the Supreme Personality said; tri-vidhā—of three kinds; bhavati—is; śhraddhā—faith; dehinām—embodied beings; sā—which; sva-bhāva-jā—born of one’s innate nature; sāttvikī—of the mode of goodness; rājasī—of the mode of passion; cha—and; eva—certainly; tāmasī—of the mode of ignorance; cha—and; iti—thus; tām—about this; śhṛiṇu—hear
+verse-2
+BG 17.2: The Supreme Divine Personality said: Every human being is born with innate faith, which can be of three kinds—sāttvic, rājasic, or tāmasic. Now hear about this from me.
+verse-2
+
+verse-3
+सत्वानुरूपा सर्वस्य श्रद्धा भवति भारत |
+श्रद्धामयोऽयं पुरुषो यो यच्छ्रद्ध: स एव स: || 3||
+verse-3
+sattvānurūpā sarvasya śhraddhā bhavati bhārata
+śhraddhā-mayo ‘yaṁ puruṣho yo yach-chhraddhaḥ sa eva saḥ
+verse-3
+sattva-anurūpā—conforming to the nature of one’s mind; sarvasya—all; śhraddhā—faith; bhavati—is; bhārata—Arjun, the scion of Bharat; śhraddhāmayaḥ—possessing faith; ayam—that; puruṣhaḥ—human being; yaḥ—who; yat-śhraddhaḥ—whatever the nature of their faith; saḥ—their; eva—verily; saḥ—they
+verse-3
+BG 17.3: The faith of all humans conforms to the nature of their mind. All people possess faith, and whatever the nature of their faith, that is verily what they are.
+verse-3
+
+verse-4
+यजन्ते सात्विका देवान्यक्षरक्षांसि राजसा: |
+प्रेतान्भूतगणांश्चान्ये यजन्ते तामसा जना: || 4||
+verse-4
+yajante sāttvikā devān yakṣha-rakṣhānsi rājasāḥ
+pretān bhūta-gaṇānśh chānye yajante tāmasā janāḥ
+verse-4
+yajante—worship; sāttvikāḥ—those in the mode of goodness; devān—celestial gods; yakṣha—semi-celestial beings who exude power and wealth; rakṣhānsi—powerful beings who embody sensual enjoyment, revenge, and wrath; rājasāḥ—those in the mode of passion; pretān-bhūta-gaṇān—ghosts and spirits; cha—and; anye—others; yajante—worship; tāmasāḥ—those in the mode of ignorance; janāḥ—persons
+verse-4
+BG 17.4: Those in the mode of goodness worship the celestial gods; those in the mode of passion worship the yakṣhas and rākṣhasas; those in the mode of ignorance worship ghosts and spirits.
+verse-4
+
+verse-5
+अशास्त्रविहितं घोरं तप्यन्ते ये तपो जना: |
+दम्भाहङ्कारसंयुक्ता: कामरागबलान्विता: || 5||
+verse-5
+aśhāstra-vihitaṁ ghoraṁ tapyante ye tapo janāḥ
+dambhāhankāra-sanyuktāḥ kāma-rāga-balānvitāḥ
+verse-5
+aśhāstra-vihitam—not enjoined by the scriptures; ghoram—stern; tapyante—perform; ye—who; tapaḥ—austerities; janāḥ—people; dambha—hypocrisy; ahankāra—egotism; sanyuktāḥ—possessed of; kāma—desire; rāga—attachment; bala—force; anvitāḥ—impelled by;
+verse-5
+BG 17.5: Some people perform stern austerities that are not enjoined by the scriptures, but rather motivated by hypocrisy and egotism. Impelled by desire and attachment,
+verse-5
+
+verse-6
+कर्षयन्त: शरीरस्थं भूतग्राममचेतस: |
+मां चैवान्त:शरीरस्थं तान्विद्ध्यासुरनिश्चयान् || 6||
+verse-6
+karṣhayantaḥ śharīra-sthaṁ bhūta-grāmam achetasaḥ
+māṁ chaivāntaḥ śharīra-sthaṁ tān viddhy āsura-niśhchayān
+verse-6
+karṣhayantaḥ—torment; śharīra-stham—within the body; bhūta-grāmam—elements of the body; achetasaḥ—senseless; mām—me; cha—and; eva—even; antaḥ—within; śharīra-stham—dwelling in the body; tān—them; viddhi—know; āsura-niśhchayān—of demoniacal resolves
+verse-6
+BG 17.6: ..they torment not only the elements of their body, but also I who dwell within them as the Supreme Soul. Know these senseless people to be of demoniacal resolves.
+verse-6
+
+verse-7
+आहारस्त्वपि सर्वस्य त्रिविधो भवति प्रिय: |
+यज्ञस्तपस्तथा दानं तेषां भेदमिमं शृणु || 7||
+verse-7
+āhāras tv api sarvasya tri-vidho bhavati priyaḥ
+yajñas tapas tathā dānaṁ teṣhāṁ bhedam imaṁ śhṛiṇu
+verse-7
+āhāraḥ—food; tu—indeed; api—even; sarvasya—of all; tri-vidhaḥ—of three kinds; bhavati—is; priyaḥ—dear; yajñaḥ—sacrifice; tapaḥ—austerity; tathā—and; dānam—charity; teṣhām—of them; bhedam—distinctions; imam—this; śhṛiṇu—hear
+verse-7
+BG 17.7: The food persons prefer is according to their dispositions. The same is true for the sacrifice, austerity, and charity they incline toward. Now hear of the distinctions from me.
+verse-7
+
+verse-8
+आयु:सत्वबलारोग्यसुखप्रीतिविवर्धना: |
+रस्या: स्निग्धा: स्थिरा हृद्या आहारा: सात्विकप्रिया: || 8||
+verse-8
+āyuḥ-sattva-balārogya-sukha-prīti-vivardhanāḥ
+rasyāḥ snigdhāḥ sthirā hṛidyā āhārāḥ sāttvika-priyāḥ
+verse-8
+āyuḥ sattva—which promote longevity; bala—strength; ārogya—health; sukha—happiness; prīti—satisfaction; vivardhanāḥ—increase; rasyāḥ—juicy; snigdhāḥ—succulent; sthirāḥ—nourishing; hṛidyāḥ—pleasing to the heart; āhārāḥ—food; sāttvika-priyāḥ—dear to those in the mode of goodness
+verse-8
+BG 17.8: Persons in the mode of goodness prefer foods that promote the life span, and increase virtue, strength, health, happiness, and satisfaction. Such foods are juicy, succulent, nourishing, and naturally tasteful.
+verse-8
+
+verse-9
+कट्वम्ललवणात्युष्णतीक्ष्णरूक्षविदाहिन: |
+आहारा राजसस्येष्टा दु:खशोकामयप्रदा: || 9||
+verse-9
+kaṭv-amla-lavaṇāty-uṣhṇa- tīkṣhṇa-rūkṣha-vidāhinaḥ
+āhārā rājasasyeṣhṭā duḥkha-śhokāmaya-pradāḥ
+verse-9
+kaṭu—bitter; amla—sour; lavaṇa—salty; ati-uṣhṇa—very hot; tīkṣhṇa—pungent; rūkṣha—dry; vidāhinaḥ—chiliful; āhārāḥ—food; rājasasya—to persons in the mode of passion; iṣhṭāḥ—dear; duḥkha—pain; śhoka—grief; āmaya—disease; pradāḥ—produce
+verse-9
+BG 17.9: Foods that are too bitter, too sour, salty, very hot, pungent, dry, and chiliful, are dear to persons in the mode of passion. Such foods produce pain, grief, and disease.
+verse-9
+
+verse-10
+यातयामं गतरसं पूति पर्युषितं च यत् |
+उच्छिष्टमपि चामेध्यं भोजनं तामसप्रियम् || 10||
+verse-10
+yāta-yāmaṁ gata-rasaṁ pūti paryuṣhitaṁ cha yat
+uchchhiṣhṭam api chāmedhyaṁ bhojanaṁ tāmasa-priyam
+verse-10
+yāta-yāmam—stale foods; gata-rasam—tasteless; pūti—putrid; paryuṣhitam—polluted; cha—and; yat—which; uchchhiṣhṭam—left over; api—also; cha—and; amedhyam—impure; bhojanam—foods; tāmasa—to persons in the mode of ignorance; priyam—dear
+verse-10
+BG 17.10: Foods that are overcooked, stale, putrid, polluted, and impure are dear to persons in the mode of ignorance.
+verse-10
+
+verse-11
+अफलाकाङ्क्षिभिर्यज्ञो विधिदृष्टो य इज्यते |
+यष्टव्यमेवेति मन: समाधाय स सात्विक: || 11||
+verse-11
+aphalākāṅkṣhibhir yajño vidhi-driṣhṭo ya ijyate
+yaṣhṭavyam eveti manaḥ samādhāya sa sāttvikaḥ
+verse-11
+aphala-ākāṅkṣhibhiḥ—without expectation of any reward; yajñaḥ—sacrifice; vidhi-driṣhṭaḥ—that is in accordance with the scriptural injunctions; yaḥ—which; ijyate—is performed; yaṣhṭavyam-eva-iti—ought to be offered; manaḥ—mind; samādhāya—with conviction; saḥ—that; sāttvikaḥ—of the nature of goodness
+verse-11
+BG 17.11: Sacrifice that is performed according to the scriptural injunctions without expectation of rewards, with the firm conviction of the mind that it is a matter of duty is of the nature of goodness.
+verse-11
+
+verse-12
+अभिसन्धाय तु फलं दम्भार्थमपि चैव यत् |
+इज्यते भरतश्रेष्ठ तं यज्ञं विद्धि राजसम् || 12||
+verse-12
+abhisandhāya tu phalaṁ dambhārtham api chaiva yat
+ijyate bharata-śhreṣhṭha taṁ yajñaṁ viddhi rājasam
+verse-12
+abhisandhāya—motivated by; tu—but; phalam—the result; dambha—pride; artham—for the sake of; api—also; cha—and; eva—certainly; yat—that which; ijyate—is performed; bharata-śhreṣhṭha—Arjun, the best of the Bharatas; tam—that; yajñam—sacrifice; viddhi—know; rājasam—in the mode of passion
+verse-12
+BG 17.12: O best of the Bharatas, know that sacrifice, which is performed for material benefit, or with hypocritical aim, to be in the mode of passion.
+verse-12
+
+verse-13
+विधिहीनमसृष्टान्नं मन्त्रहीनमदक्षिणम् |
+श्रद्धाविरहितं यज्ञं तामसं परिचक्षते || 13||
+verse-13
+vidhi-hīnam asṛiṣhṭānnaṁ mantra-hīnam adakṣhiṇam
+śhraddhā-virahitaṁ yajñaṁ tāmasaṁ parichakṣhate
+verse-13
+vidhi-hīnam—without scriptural direction; asṛiṣhṭa-annam—without distribution of prasādam; mantra-hīnam—with no chanting of the Vedic hymns; adakṣhiṇam—with no remunerations to the priests; śhraddhā—faith; virahitam—without; yajñam—sacrifice; tāmasam—in the mode of ignorance; parichakṣhate—is to be considered
+verse-13
+BG 17.13: Sacrifice devoid of faith and contrary to the injunctions of the scriptures, in which no food is offered, no mantras chanted, and no donation made, is to be considered in the mode of ignorance.
+verse-13
+
+verse-14
+देवद्विजगुरुप्राज्ञपूजनं शौचमार्जवम् |
+ब्रह्मचर्यमहिंसा च शारीरं तप उच्यते || 14||
+verse-14
+deva-dwija-guru-prājña- pūjanaṁ śhaucham ārjavam
+brahmacharyam ahinsā cha śhārīraṁ tapa uchyate
+verse-14
+deva—the Supreme Lord; dwija—the Brahmins; guru—the spiritual master; prājña—the elders; pūjanam—worship; śhaucham—cleanliness; ārjavam—simplicity; brahmacharyam—celibacy; ahinsā—non-violence; cha—and; śhārīram—of the body; tapaḥ—austerity; uchyate—is declared as
+verse-14
+BG 17.14: Worship of the Supreme Lord, the Brahmins, the spiritual master, the wise, and the elders—when this is done with the observance of cleanliness, simplicity, celibacy, and non-violence—is declared as the austerity of the body.
+verse-14
+
+verse-15
+अनुद्वेगकरं वाक्यं सत्यं प्रियहितं च यत् |
+स्वाध्यायाभ्यसनं चैव वाङ्मयं तप उच्यते || 15||
+verse-15
+anudvega-karaṁ vākyaṁ satyaṁ priya-hitaṁ cha yat
+svādhyāyābhyasanaṁ chaiva vāṅ-mayaṁ tapa uchyate
+verse-15
+anudvega-karam—not causing distress; vākyam—words; satyam—truthful; priya- hitam—beneficial; cha—and; yat—which; svādhyāya-abhyasanam—recitation of the Vedic scriptures; cha eva—as well as; vāṅ-mayam—of speech; tapaḥ—austerity; uchyate—are declared as
+verse-15
+BG 17.15: Words that do not cause distress, are truthful, inoffensive, and beneficial, as well as the regular recitation of the Vedic scriptures—these are declared as the austerity of speech.
+verse-15
+
+verse-16
+मन: प्रसाद: सौम्यत्वं मौनमात्मविनिग्रह: |
+भावसंशुद्धिरित्येतत्तपो मानसमुच्यते || 16||
+verse-16
+manaḥ-prasādaḥ saumyatvaṁ maunam ātma-vinigrahaḥ
+bhāva-sanśhuddhir ity etat tapo mānasam uchyate
+verse-16
+manaḥ-prasādaḥ—serenity of thought; saumyatvam—gentleness; maunam—silence; ātma-vinigrahaḥ—self-control; bhāva-sanśhuddhiḥ—purity of purpose; iti—thus; etat—these; tapaḥ—austerity; mānasam—of the mind; uchyate—are declared as
+verse-16
+BG 17.16: Serenity of thought, gentleness, silence, self-control, and purity of purpose—all these are declared as the austerity of the mind.
+verse-16
+
+verse-17
+श्रद्धया परया तप्तं तपस्तत्त्रिविधं नरै: |
+अफलाकाङ्क्षिभिर्युक्तै: सात्विकं परिचक्षते || 17||
+verse-17
+śhraddhayā parayā taptaṁ tapas tat tri-vidhaṁ naraiḥ
+aphalākāṅkṣhibhir yuktaiḥ sāttvikaṁ parichakṣhate
+verse-17
+śhraddhayā—with faith; parayā—transcendental; taptam—practiced; tapaḥ—austerity; tat—that; tri-vidham—three-fold; naraiḥ—by persons; aphala-ākāṅkṣhibhiḥ—without yearning for material rewards; yuktaiḥ—steadfast; sāttvikam—in the mode of goodness; parichakṣhate—are designated
+verse-17
+BG 17.17: When devout persons with ardent faith practice these three-fold austerities without yearning for material rewards, they are designated as austerities in the mode of goodness.
+verse-17
+
+verse-18
+सत्कारमानपूजार्थं तपो दम्भेन चैव यत् |
+क्रियते तदिह प्रोक्तं राजसं चलमध्रुवम् || 18||
+verse-18
+satkāra-māna-pūjārthaṁ tapo dambhena chaiva yat
+kriyate tad iha proktaṁ rājasaṁ chalam adhruvam
+verse-18
+sat-kāra—respect; māna—honor; pūjā—adoration; artham—for the sake of; tapaḥ—austerity; dambhena—with ostentation; cha—also; eva—certainly; yat—which; kriyate—is performed; tat—that; iha—in this world; proktam—is said; rājasam—in the mode of passion; chalam—flickering; adhruvam—temporary
+verse-18
+BG 17.18: Austerity that is performed with ostentation for the sake of gaining honor, respect, and adoration is in the mode of passion. Its benefits are unstable and transitory.
+verse-18
+
+verse-19
+मूढग्राहेणात्मनो यत्पीडया क्रियते तप: |
+परस्योत्सादनार्थं वा तत्तामसमुदाहृतम् || 19||
+verse-19
+mūḍha-grāheṇātmano yat pīḍayā kriyate tapaḥ
+parasyotsādanārthaṁ vā tat tāmasam udāhṛitam
+verse-19
+mūḍha—those with confused notions; grāheṇa—with endeavor; ātmanaḥ—one’s own self; yat—which; pīḍayā—torturing; kriyate—is performed; tapaḥ—austerity; parasya—of others; utsādana-artham—for harming; vā—or; tat—that; tāmasam—in the mode of ignorance; udāhṛitam—is described to be
+verse-19
+BG 17.19: Austerity that is performed by those with confused notions, and which involves torturing the self or harming others, is described to be in the mode of ignorance.
+verse-19
+
+verse-20
+दातव्यमिति यद्दानं दीयतेऽनुपकारिणे |
+देशे काले च पात्रे च तद्दानं सात्विकं स्मृतम् || 20||
+verse-20
+dātavyam iti yad dānaṁ dīyate ‘nupakāriṇe
+deśhe kāle cha pātre cha tad dānaṁ sāttvikaṁ smṛitam
+verse-20
+dātavyam—worthy of charity; iti—thus; yat—which; dānam—charity; dīyate—is given; anupakāriṇe—to one who cannot give in return; deśhe—in the proper place; kāle—at the proper time; cha—and; pātre—to a worthy person; cha—and; tat—that; dānam—charity; sāttvikam—in the mode of goodness; smṛitam—is stated to be
+verse-20
+BG 17.20: Charity given to a worthy person simply because it is right to give, without consideration of anything in return, at the proper time and in the proper place, is stated to be in the mode of goodness.
+verse-20
+
+verse-21
+यत्तु प्रत्युपकारार्थं फलमुद्दिश्य वा पुन: |
+दीयते च परिक्लिष्टं तद्दानं राजसं स्मृतम् || 21||
+verse-21
+yat tu pratyupakārārthaṁ phalam uddiśhya vā punaḥ
+dīyate cha parikliṣhṭaṁ tad dānaṁ rājasaṁ smṛitam
+verse-21
+yat—which; tu—but; prati-upakāra-artham—with the hope of a return; phalam—reward; uddiśhya—expectation; vā—or; punaḥ—again; dīyate—is given; cha—and; parikliṣhṭam—reluctantly; tat—that; dānam—charity; rājasam—in the mode of passion; smṛitam—is said to be
+verse-21
+BG 17.21: But charity given with reluctance, with the hope of a return or in expectation of a reward, is said to be in the mode of passion.
+verse-21
+
+verse-22
+अदेशकाले यद्दानमपात्रेभ्यश्च दीयते |
+असत्कृतमवज्ञातं तत्तामसमुदाहृतम् || 22||
+verse-22
+adeśha-kāle yad dānam apātrebhyaśh cha dīyate
+asat-kṛitam avajñātaṁ tat tāmasam udāhṛitam
+verse-22
+adeśha—at the wrong place; kāle—at the wrong time; yat—which; dānam—charity; apātrebhyaḥ—to unworthy persons; cha—and; dīyate—is given; asat-kṛitam—without respect; avajñātam—with contempt; tat—that; tāmasam—of the nature of nescience; udāhṛitam—is held to be
+verse-22
+BG 17.22: And that charity, which is given at the wrong place and wrong time to unworthy persons, without showing respect, or with contempt, is held to be of the nature of nescience.
+verse-22
+
+verse-23
+ॐ तत्सदिति निर्देशो ब्रह्मणस्त्रिविध: स्मृत: |
+ब्राह्मणास्तेन वेदाश्च यज्ञाश्च विहिता: पुरा || 23||
+verse-23
+oṁ tat sad iti nirdeśho brahmaṇas tri-vidhaḥ smṛitaḥ
+brāhmaṇās tena vedāśh cha yajñāśh cha vihitāḥ purā
+verse-23
+om tat sat—syllables representing aspects of transcendence; iti—thus; nirdeśhaḥ—symbolic representatives; brahmaṇaḥ—the Supreme Absolute Truth; tri-vidhaḥ—of three kinds; smṛitaḥ—have been declared; brāhmaṇāḥ—the priests; tena—from them; vedāḥ—scriptures; cha—and; yajñāḥ—sacrifice; cha—and; vihitāḥ—came about; purā—from the beginning of creation
+verse-23
+BG 17.23: The words “Om Tat Sat” have been declared as symbolic representations of the Supreme Absolute Truth, from the beginning of creation. From them came the priests, scriptures, and sacrifice.
+verse-23
+
+verse-24
+तस्माद् ॐ इत्युदाहृत्य यज्ञदानतप:क्रिया: |
+प्रवर्तन्ते विधानोक्ता: सततं ब्रह्मवादिनाम् || 24||
+verse-24
+tasmād oṁ ity udāhṛitya yajña-dāna-tapaḥ-kriyāḥ
+pravartante vidhānoktāḥ satataṁ brahma-vādinām
+verse-24
+tasmāt—therefore; om—sacred syllable om; iti—thus; udāhṛitya—by uttering; yajña—sacrifice; dāna—charity; tapaḥ—penance; kriyāḥ—performing; pravartante—begin; vidhāna-uktāḥ—according to the prescriptions of Vedic injunctions; satatam—always; brahma-vādinām—expounders of the Vedas
+verse-24
+BG 17.24: Therefore, when performing acts of sacrifice, offering charity, or undertaking penance, expounders of the Vedas always begin by uttering “Om” according to the prescriptions of Vedic injunctions.
+verse-24
+
+verse-25
+तदित्यनभिसन्धाय फलं यज्ञतप:क्रिया: |
+दानक्रियाश्च विविधा: क्रियन्ते मोक्षकाङ्क्षिभि: || 25||
+verse-25
+tad ity anabhisandhāya phalaṁ yajña-tapaḥ-kriyāḥ
+dāna-kriyāśh cha vividhāḥ kriyante mokṣha-kāṅkṣhibhiḥ
+verse-25
+tat—the syllable Tat; iti—thus; anabhisandhāya—without desiring; phalam—fruitive rewards; yajña—sacrifice; tapaḥ—austerity; kriyāḥ—acts; dāna—charity; kriyāḥ—acts; cha—and; vividhāḥ—various; kriyante—are done; mokṣha-kāṅkṣhibhiḥ—by seekers of freedom from material entanglements
+verse-25
+BG 17.25: Persons who do not desire fruitive rewards, but seek to be free from material entanglements, utter the word “Tat” along with acts of austerity, sacrifice, and charity.
+verse-25
+
+verse-26
+सद्भावे साधुभावे च सदित्येतत्प्रयुज्यते |
+प्रशस्ते कर्मणि तथा सच्छब्द: पार्थ युज्यते || 26||
+verse-26
+sad-bhāve sādhu-bhāve cha sad ity etat prayujyate
+praśhaste karmaṇi tathā sach-chhabdaḥ pārtha yujyate
+verse-26
+sat-bhāve—with the intention of eternal existence and goodness; sādhu-bhāve—with auspicious intention; cha—also; sat—the syllable Sat; iti—thus; etat—this; prayujyate—is used; praśhaste—auspicious; karmaṇi—action; tathā—also; sat-śhabdaḥ—the word “Sat”; pārtha—Arjun, the son of Pritha; yujyate—is used;
+verse-26
+BG 17.26: The word “Sat” means eternal existence and goodness. O Arjun, it is also used to describe an auspicious action.
+verse-26
+
+verse-27
+यज्ञे तपसि दाने च स्थिति: सदिति चोच्यते |
+कर्म चैव तदर्थीयं सदित्येवाभिधीयते || 27||
+verse-27
+yajñe tapasi dāne cha sthitiḥ sad iti chochyate
+karma chaiva tad-arthīyaṁ sad ity evābhidhīyate
+verse-27
+yajñe—in sacrifice; tapasi—in penance; dāne—in charity; cha—and; sthitiḥ—established in steadiness; sat—the syllable Sat; iti—thus; cha—and; uchyate—is pronounced; karma—action; cha—and; eva—indeed; tat-arthīyam—for such purposes; sat—the syllable Sat; iti—thus; eva—indeed; abhidhīyate—is described
+verse-27
+BG 17.27: Being established in the performance of sacrifice, penance, and charity, is also described by the word “Sat.” And so any act for such purposes is named “Sat.”
+verse-27
+
+verse-28
+अश्रद्धया हुतं दत्तं तपस्तप्तं कृतं च यत् |
+असदित्युच्यते पार्थ न च तत्प्रेत्य नो इह || 28||
+verse-28
+aśhraddhayā hutaṁ dattaṁ tapas taptaṁ kṛitaṁ cha yat
+asad ity uchyate pārtha na cha tat pretya no iha
+verse-28
+aśhraddhayā—without faith; hutam—sacrifice; dattam—charity; tapaḥ—penance; taptam—practiced; kṛitam—done; cha—and; yat—which; asat—perishable; iti—thus; uchyate—are termed as; pārtha—Arjun, the son of Pritha; na—not; cha—and; tat—that; pretya—in the next world; na u—not; iha—in this world
+verse-28
+BG 17.28: O son of Pritha, whatever acts of sacrifice or penance are done without faith, are termed as “Asat.” They are useless both in this world and the next.
+verse-28
+
+</body>
+</html>
