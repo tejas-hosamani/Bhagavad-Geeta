dbedfae8ba38a4fa04192f1f8a0fc705d224d66e
CodingCreature <CodingCreate101@users.noreply.github.com>
2017-10-14 14:42:49 +0530

    Chapter 16 completed. Open for review.

---
+++ chapter16.php
@@ -0,0 +1,301 @@
+<html>
+<title>BG Chapter 16</title>
+<body>
+    
+<h1>Chapter 16: Daivāsura Sampada Vibhāga Yoga</h1>
+
+verse-1
+श्रीभगवानुवाच |
+अभयं सत्वसंशुद्धिर्ज्ञानयोगव्यवस्थिति: |
+दानं दमश्च यज्ञश्च स्वाध्यायस्तप आर्जवम् || 1||
+verse-1
+śhrī-bhagavān uvācha
+abhayaṁ sattva-sanśhuddhir jñāna-yoga-vyavasthitiḥ
+dānaṁ damaśh cha yajñaśh cha svādhyāyas tapa ārjavam
+verse-1
+śhrī-bhagavān uvācha—the Supreme Divine Personality said; abhayam—fearlessness; sattva-sanśhuddhiḥ—purity of mind; jñāna—knowledge; yoga—spiritual; vyavasthitiḥ—steadfastness; dānam—charity; damaḥ—control of the senses; cha—and; yajñaḥ—performance of sacrifice; cha—and; svādhyāyaḥ—study of sacred books; tapaḥ—austerity; ārjavam—straightforwardness;
+verse-1
+BG 16.1: The Supreme Divine Personality said: These are the saintly virtues of those endowed with a divine nature—fearlessness, purity of mind, steadfastness in spiritual knowledge, charity, control of the senses, performance of sacrifice, study of the sacred books, austerity
+verse-1
+
+verse-2
+अहिंसा सत्यमक्रोधस्त्याग: शान्तिरपैशुनम् |
+दया भूतेष्वलोलुप्त्वं मार्दवं ह्रीरचापलम् || 2||
+verse-2
+ahinsā satyam akrodhas tyāgaḥ śhāntir apaiśhunam
+dayā bhūteṣhv aloluptvaṁ mārdavaṁ hrīr achāpalam
+verse-2
+ahinsā—non-violence; satyam—truthfulness; akrodhaḥ—absence of anger; tyāgaḥ—renunciation; śhāntiḥ—peacefulness; apaiśhunam—restraint from fault-finding; dayā—compassion; bhūteṣhu—toward all living beings; aloluptvam—absence of covetousness; mārdavam—gentleness; hrīḥ—modesty; achāpalam—lack of fickleness;
+verse-2
+BG 16.2: ..and straightforwardness; non-violence, truthfulness, absence of anger, renunciation, peacefulness, restraint from fault-finding, compassion toward all living beings, absence of covetousness, gentleness, modesty, and lack of fickleness;
+verse-2
+
+verse-3
+तेज: क्षमा धृति: शौचमद्रोहोनातिमानिता |
+भवन्ति सम्पदं दैवीमभिजातस्य भारत || 3||
+verse-3
+tejaḥ kṣhamā dhṛitiḥ śhaucham adroho nāti-mānitā
+bhavanti sampadaṁ daivīm abhijātasya bhārata
+verse-3
+tejaḥ—vigor; kṣhamā—forgiveness; dhṛitiḥ—fortitude; śhaucham—cleanliness; adrohaḥ—bearing enmity toward none; na—not; ati-mānitā—absence of vanity; bhavanti—are; sampadam—qualities; daivīm—godly; abhijātasya—of those endowed with; bhārata—scion of Bharat
+verse-3
+BG 16.3: ..vigor, forgiveness, fortitude, cleanliness, bearing enmity toward none, and absence of vanity, O scion of Bharat.
+verse-3
+
+verse-4
+दम्भो दर्पोऽभिमानश्च क्रोध: पारुष्यमेव च |
+अज्ञानं चाभिजातस्य पार्थ सम्पदमासुरीम् || 4||
+verse-4
+dambho darpo ’bhimānaśh cha krodhaḥ pāruṣhyam eva cha
+ajñānaṁ chābhijātasya pārtha sampadam āsurīm
+verse-4
+dambhaḥ—hypocrisy; darpaḥ—arrogance; abhimānaḥ—conceit; cha—and; krodhaḥ—anger; pāruṣhyam—harshness; eva—certainly; cha—and; ajñānam—ignorance; cha—and; abhijātasya—of those who possess; pārtha—Arjun, the son of Pritha; sampadam—qualities; āsurīm—demoniac
+verse-4
+BG 16.4: O Parth, the qualities of those who possess a demoniac nature are hypocrisy, arrogance, conceit, anger, harshness, and ignorance.
+verse-4
+
+verse-5
+दैवी सम्पद्विमोक्षाय निबन्धायासुरी मता |
+मा शुच: सम्पदं दैवीमभिजातोऽसि पाण्डव || 5||
+verse-5
+daivī sampad vimokṣhāya nibandhāyāsurī matā
+mā śhuchaḥ sampadaṁ daivīm abhijāto ’si pāṇḍava
+verse-5
+daivī—divine; sampat—qualities; vimokṣhāya—toward liberation; nibandhāya—to bondage; āsurī—demoniac qualities; matā—are considered; mā—do not; śhuchaḥ—grieve; sampadam—virtues; daivīm—saintly; abhijātaḥ—born; asi—you are; pāṇḍava—Arjun, the son of Pandu
+verse-5
+BG 16.5: The divine qualities lead to liberation, while the demoniac qualities are the cause for a continuing destiny of bondage. Grieve not, O Arjun, as you were born with saintly virtues.
+verse-5
+
+verse-6
+द्वौ भूतसर्गौ लोकेऽस्मिन्दैव आसुर एव च |
+दैवो विस्तरश: प्रोक्त आसुरं पार्थ मे शृणु || 6||
+verse-6
+dvau bhūta-sargau loke ’smin daiva āsura eva cha
+daivo vistaraśhaḥ prokta āsuraṁ pārtha me śhṛiṇu
+verse-6
+dvau—two; bhūta-sargau—of created living beings; loke—in the world; asmin—this; daivaḥ—divine; āsuraḥ—demoniac; eva—certainly; cha—and; daivaḥ—the divine; vistaraśhaḥ—at great length; proktaḥ—said; āsuram—the demoniac; pārtha—Arjun, the son of Pritha; me—from me; śhṛiṇu—hear
+verse-6
+BG 16.6: There are two kinds of beings in this world—those endowed with a divine nature and those possessing a demoniac nature. I have described the divine qualities in detail, O Arjun. Now hear from me about the demoniac nature.
+verse-6
+
+verse-7
+प्रवृत्तिं च निवृत्तिं च जना न विदुरासुरा: |
+न शौचं नापि चाचारो न सत्यं तेषु विद्यते || 7||
+verse-7
+pravṛittiṁ cha nivṛittiṁ cha janā na vidur āsurāḥ
+na śhauchaṁ nāpi chāchāro na satyaṁ teṣhu vidyate
+verse-7
+pravṛittim—proper actions; cha—and; nivṛittim—improper actions; cha—and; janāḥ—persons; na—not; viduḥ—comprehend; āsurāḥ—those possessing demoniac nature; na—neither; śhaucham—purity; na—nor; api—even; cha—and; āchāraḥ—conduct; na—nor; satyam—truthfulness; teṣhu—in them; vidyate—exist
+verse-7
+BG 16.7: Those possessing a demoniac nature do not comprehend what actions are proper and what are improper. Hence, they possess neither purity, nor good conduct, nor even truthfulness.
+verse-7
+
+verse-8
+असत्यमप्रतिष्ठं ते जगदाहुरनीश्वरम् |
+अपरस्परसम्भूतं किमन्यत्कामहैतुकम् || 8||
+verse-8
+asatyam apratiṣhṭhaṁ te jagad āhur anīśhvaram
+aparaspara-sambhūtaṁ kim anyat kāma-haitukam
+verse-8
+asatyam—without absolute truth; apratiṣhṭham—without any basis; te—they; jagat—the world; āhuḥ—say; anīśhvaram—without a God; aparaspara—without cause; sambhūtam—created; kim—what; anyat—other; kāma-haitukam—for sexual gratification only
+verse-8
+BG 16.8: They say, “The world is without absolute truth, without any basis (for moral order), and without a God (who has created or is controlling it). It is created from the combination of the two sexes, and has no purpose other than sexual gratification.”
+verse-8
+
+verse-9
+एतां दृष्टिमवष्टभ्य नष्टात्मानोऽल्पबुद्धय: |
+प्रभवन्त्युग्रकर्माण: क्षयाय जगतोऽहिता: || 9||
+verse-9
+etāṁ dṛiṣhṭim avaṣhṭabhya naṣhṭātmāno ’lpa-buddhayaḥ
+prabhavanty ugra-karmāṇaḥ kṣhayāya jagato ’hitāḥ
+verse-9
+etām—such; dṛiṣhṭim—views; avaṣhṭabhya—holding; naṣhṭa—misdirected; ātmānaḥ—souls; alpa-buddhayaḥ—of small intellect; prabhavanti—arise; ugra—cruel; karmāṇaḥ—actions; kṣhayāya—destruction; jagataḥ—of the world; ahitāḥ—enemies
+verse-9
+BG 16.9: Holding fast to such views, these misdirected souls, with small intellect and cruel actions, arise as enemies of the world threatening its destruction.
+verse-9
+
+verse-10
+काममाश्रित्य दुष्पूरं दम्भमानमदान्विता: |
+मोहाद्गृहीत्वासद्ग्राहान्प्रवर्तन्तेऽशुचिव्रता: || 10||
+verse-10
+kāmam āśhritya duṣhpūraṁ dambha-māna-madānvitāḥ
+mohād gṛihītvāsad-grāhān pravartante ’śhuchi-vratāḥ
+verse-10
+kāmam—lust; āśhritya—harboring; duṣhpūram—insatiable; dambha—hypocrisy; māna—arrogance; mada-anvitāḥ—clinging to false tenets; mohāt—the illusioned; gṛihītvā—being attracted to; asat—impermanent; grāhān—things; pravartante—they flourish; aśhuchi-vratāḥ—with impure resolve
+verse-10
+BG 16.10: Harboring insatiable lust, full of hypocrisy, pride and arrogance, the demoniac cling to their false tenets. Thus illusioned, they are attracted to the impermanent and work with impure resolve.
+verse-10
+
+verse-11
+चिन्तामपरिमेयां च प्रलयान्तामुपाश्रिता: |
+कामोपभोगपरमा एतावदिति निश्चिता: || 11||
+verse-11
+chintām aparimeyāṁ cha pralayāntām upāśhritāḥ
+kāmopabhoga-paramā etāvad iti niśhchitāḥ
+verse-11
+chintām—anxieties; aparimeyām—endless; cha—and; pralaya-antām—until death; upāśhritāḥ—taking refuge; kāma-upabhoga—gratification of desires; paramāḥ—the purpose of life; etāvat—still; iti—thus; niśhchitāḥ—with complete assurance
+verse-11
+BG 16.11: They are obsessed with endless anxieties that end only with death. Still, they maintain with complete assurance that gratification of desires and accumulation of wealth is the highest purpose of life.
+verse-11
+
+verse-12
+आशापाशशतैर्बद्धा: कामक्रोधपरायणा: |
+ईहन्ते कामभोगार्थमन्यायेनार्थसञ्जयान् || 12||
+verse-12
+āśhā-pāśha-śhatair baddhāḥ kāma-krodha-parāyaṇāḥ
+īhante kāma-bhogārtham anyāyenārtha-sañchayān
+verse-12
+āśhā-pāśha—bondage of desires; śhataiḥ—by hundreds; baddhāḥ—bound; kāma—lust; krodha—anger; parāyaṇāḥ—dedicated to; īhante—strive; kāma—lust; bhoga—gratification of the senses; artham—for; anyāyena—by unjust means; artha—wealth; sañchayān—to accumulate
+verse-12
+BG 16.12: Held in bondage by hundreds of desires, and driven by lust and anger, they strive to accumulate wealth by unjust means, all for the gratification of their senses.
+verse-12
+
+verse-13
+इदमद्य मया लब्धमिमं प्राप्स्ये मनोरथम् |
+इदमस्तीदमपि मे भविष्यति पुनर्धनम् || 13||
+verse-13
+idam adya mayā labdham imaṁ prāpsye manoratham
+idam astīdam api me bhaviṣhyati punar dhanam
+verse-13
+idam—this; adya—today; mayā—by me; labdham—gained; imam—this; prāpsye—I shall acquire; manaḥ-ratham—desire; idam—this; asti—is; idam—this; api—also; me—mine; bhaviṣhyati—in future; punaḥ—again; dhanam—wealth;
+verse-13
+BG 16.13: The demoniac persons think, “I have gained so much wealth today, and I shall now fulfill this desire of mine. This is mine, and tomorrow I shall have even more. 
+verse-13
+
+
+
+
+verse-14
+असौ मया हत: शत्रुर्हनिष्ये चापरानपि |
+ईश्वरोऽहमहं भोगी सिद्धोऽहं बलवान्सुखी || 14||
+verse-14
+asau mayā hataḥ śhatrur haniṣhye chāparān api
+īśhvaro ’ham ahaṁ bhogī siddho ’haṁ balavān sukhī
+verse-14
+asau—that; mayā—by me; hataḥ—has been destroyed; śhatruḥ—enemy; haniṣhye—I shall destroy; cha—and; aparān—others; api—also; īśhvaraḥ—God; aham—I; aham—I; bhogī—the enjoyer; siddhaḥ—powerful; aham—I; bala-vān—powerful; sukhī—happy;
+verse-14
+BG 16.14: That enemy has been destroyed by me, and I shall destroy the others too! I am like God himself, I am the enjoyer, I am powerful, and I am happy.
+verse-14
+
+verse-15
+आढ्योऽभिजनवानस्मि कोऽन्योऽस्ति सदृशो मया |
+यक्ष्ये दास्यामि मोदिष्य इत्यज्ञानविमोहिता: || 15||
+verse-15
+āḍhyo ’bhijanavān asmi ko ’nyo ’sti sadṛiśho mayā
+yakṣhye dāsyāmi modiṣhya ity ajñāna-vimohitāḥ
+verse-15
+āḍhyaḥ—wealthy; abhijana-vān—having highly placed relatives; asmi—me; kaḥ—who; anyaḥ—else; asti—is; sadṛiśhaḥ—like; mayā—to me; yakṣhye—I shall perform sacrifices; dāsyāmi—I shall give alms; modiṣhye—I shall rejoice; iti—thus; ajñāna—ignorance; vimohitāḥ—deluded
+verse-15
+BG 16.15: I am wealthy and I have highly placed relatives. Who else is equal to me? I shall perform sacrifices (to the celestial gods); I shall give alms; I shall rejoice.” In this way, they are deluded by ignorance.
+verse-15
+
+verse-16
+अनेकचित्तविभ्रान्ता मोहजालसमावृता: |
+प्रसक्ता: कामभोगेषु पतन्ति नरकेऽशुचौ || 16||
+verse-16
+aneka-chitta-vibhrāntā moha-jāla-samāvṛitāḥ
+prasaktāḥ kāma-bhogeṣhu patanti narake ’śhuchau
+verse-16
+aneka—many; chitta—imaginings; vibhrāntāḥ—led astray; moha—delusion; jāla—mesh; samāvṛitāḥ—enveloped; prasaktāḥ—addicted; kāma-bhogeṣhu—gratification of sensuous pleasures; patanti—descend; narake—to hell; aśhuchau—murky
+verse-16
+BG 16.16: Possessed and led astray by such imaginings, enveloped in a mesh of delusion, and addicted to the gratification of sensuous pleasures, they descend to the murkiest hell.
+verse-16
+
+verse-17
+आत्मसम्भाविता: स्तब्धा धनमानमदान्विता: |
+यजन्ते नामयज्ञैस्ते दम्भेनाविधिपूर्वकम् || 17||
+verse-17
+ātma-sambhāvitāḥ stabdhā dhana-māna-madānvitāḥ
+yajante nāma-yajñais te dambhenāvidhi-pūrvakam
+verse-17
+ātma-sambhāvitāḥ—self-conceited; stabdhāḥ—stubborn; dhana—wealth; māna—pride; mada—arrogance; anvitāḥ—full of; yajante—perform sacrifice; nāma—in name only; yajñaiḥ—sacrifices; te—they; dambhena—ostentatiously; avidhi-pūrvakam—with no regards to the rules of the scriptures
+verse-17
+BG 16.17: Such self-conceited and stubborn people, full of pride and arrogant in their wealth, perform ostentatious sacrifices in name only, with no regard to the rules of the scriptures.
+verse-17
+
+verse-18
+अहङ्कारं बलं दर्पं कामं क्रोधं च संश्रिता: |
+मामात्मपरदेहेषु प्रद्विषन्तोऽभ्यसूयका: || 18||
+verse-18
+ahankāraṁ balaṁ darpaṁ kāmaṁ krodhaṁ cha sanśhritāḥ
+mām ātma-para-deheṣhu pradviṣhanto ’bhyasūyakāḥ
+verse-18
+ahankāram—egotism; balam—strength; darpam—arrogance; kāmam—desire; krodham—anger; cha—and; sanśhritāḥ—covered by; mām—me; ātma-para-deheṣhu—within one’s own and bodies of others; pradviṣhantaḥ—abuse; abhyasūyakāḥ—the demoniac
+verse-18
+BG 16.18: Blinded by egotism, strength, arrogance, desire, and anger, the demonic abuse my presence within their own body and in the bodies of others.
+verse-18
+
+verse-19
+तानहं द्विषत: क्रूरान्संसारेषु नराधमान् |
+क्षिपाम्यजस्रमशुभानासुरीष्वेव योनिषु || 19||
+verse-19
+tān ahaṁ dviṣhataḥ krūrān sansāreṣhu narādhamān
+kṣhipāmy ajasram aśhubhān āsurīṣhv eva yoniṣhu
+verse-19
+tān—these; aham—I; dviṣhataḥ—hateful; krūrān—cruel; sansāreṣhu—in the material world; nara-adhamān—the vile and vicious of humankind; kṣhipāmi—I hurl; ajasram—again and again; aśhubhān—inauspicious; āsurīṣhu—demoniac; eva—indeed; yoniṣhu—in to the wombs;
+verse-19
+BG 16.19: These cruel and hateful persons, the vile and vicious of humankind, I constantly hurl into the wombs.. 
+verse-19
+
+verse-20
+आसुरीं योनिमापन्ना मूढा जन्मनि जन्मनि |
+मामप्राप्यैव कौन्तेय ततो यान्त्यधमां गतिम् || 20||
+verse-20
+āsurīṁ yonim āpannā mūḍhā janmani janmani
+mām aprāpyaiva kaunteya tato yānty adhamāṁ gatim
+verse-20
+āsurīm—demoniac; yonim—wombs; āpannāḥ—gaining; mūḍhāḥ—the ignorant; janmani janmani—in birth after birth; mām—me; aprāpya—failing to reach; eva—even; kaunteya—Arjun, the son of Kunti; tataḥ—thereafter; yānti—go; adhamām—abominable; gatim—destination
+verse-20
+BG 16.20: ..of those with similar demoniac natures in the cycle of rebirth in the material world. These ignorant souls take birth again and again in demoniac wombs. Failing to reach me, O Arjun, they gradually sink to the most abominable type of existence.
+verse-20
+
+verse-21
+त्रिविधं नरकस्येदं द्वारं नाशनमात्मन: |
+काम: क्रोधस्तथा लोभस्तस्मादेतत्त्रयं त्यजेत् || 21||
+verse-21
+tri-vidhaṁ narakasyedaṁ dvāraṁ nāśhanam ātmanaḥ
+kāmaḥ krodhas tathā lobhas tasmād etat trayaṁ tyajet
+verse-21
+tri-vidham—three types of; narakasya—to the hell; idam—this; dvāram—gates; nāśhanam—destruction; ātmanaḥ—self; kāmaḥ—lust; krodhaḥ—anger; tathā—and; lobhaḥ—greed; tasmāt—therefore; etat—these; trayam—three; tyajet—should abandon
+verse-21
+BG 16.21: There are three gates leading to the hell of self-destruction for the soul—lust, anger, and greed. Therefore, all should abandon these three.
+verse-21
+
+verse-22
+एतैर्विमुक्त: कौन्तेय तमोद्वारैस्त्रिभिर्नर: |
+आचरत्यात्मन: श्रेयस्ततो याति परां गतिम् || 22||
+verse-22
+etair vimuktaḥ kaunteya tamo-dvārais tribhir naraḥ
+ācharaty ātmanaḥ śhreyas tato yāti parāṁ gatim
+verse-22
+etaiḥ—from this; vimuktaḥ—freed; kaunteya—Arjun, the son of Kunti; tamaḥ-dvāraiḥ—gates to darkness; tribhiḥ—three; naraḥ—a person; ācharati—endeavor; ātmanaḥ—soul; śhreyaḥ—welfare; tataḥ—thereby; yāti—attain; parām—supreme; gatim—goal
+verse-22
+BG 16.22: Those who are freed from the three gates to darkness endeavor for the welfare of their soul, and thereby attain the supreme goal.
+verse-22
+
+verse-23
+य: शास्त्रविधिमुत्सृज्य वर्तते कामकारत: |
+न स सिद्धिमवाप्नोति न सुखं न परां गतिम् || 23||
+verse-23
+yaḥ śhāstra-vidhim utsṛijya vartate kāma-kārataḥ
+na sa siddhim avāpnoti na sukhaṁ na parāṁ gatim
+verse-23
+yaḥ—who; śhāstra-vidhim—scriptural injunctions; utsṛijya—discarding; vartate—act; kāma-kārataḥ—under the impulse of desire; na—neither; saḥ—they; siddhim—perfection; avāpnoti—attain; na—nor; sukham—happiness; na—nor; parām—the supreme; gatim—goal
+verse-23
+BG 16.23: Those who act under the impulse of desire, discarding the injunctions of the scriptures, attain neither perfection, nor happiness, nor the supreme goal in life.
+verse-23
+
+verse-24
+तस्माच्छास्त्रं प्रमाणं ते कार्याकार्यव्यवस्थितौ |
+ज्ञात्वा शास्त्रविधानोक्तं कर्म कर्तुमिहार्हसि || 24||
+verse-24
+tasmāch chhāstraṁ pramāṇaṁ te kāryākārya-vyavasthitau
+jñātvā śhāstra-vidhānoktaṁ karma kartum ihārhasi
+verse-24
+tasmāt—therefore; śhāstram—scriptures; pramāṇam—authority; te—your; kārya—duty; akārya—forbidden action; vyavasthitau—in determining; jñātvā—having understood; śhāstra—scriptures; vidhāna—injunctions; uktam—as revealed; karma—actions; kartum—perform; iha—in this world; arhasi—you should
+verse-24
+BG 16.24: Therefore, let the scriptures be your authority in determining what should be done and what should not be done. Understand the scriptural injunctions and teachings, and then perform your actions in this world accordingly.
+verse-24
+
+</body>
+</html>
