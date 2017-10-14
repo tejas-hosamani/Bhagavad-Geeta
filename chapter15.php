52daa837599fe76f716a5676aa176c3ec6fe569a
CodingCreature <CodingCreate101@users.noreply.github.com>
2017-10-14 12:07:38 +0530

    Chapter 15: Needs work on Translation.
    Thank you Buzz.

---
+++ chapter15.php
@@ -0,0 +1,321 @@
+<html>
+<title>BG Chapter 15</title>
+<body>
+    
+<h1>Chapter 15: Puruṣhottama Yoga</h1>
+
+
+
+Verse-1
+श्रीभगवानुवाच |
+ऊर्ध्वमूलमध:शाखमश्वत्थं प्राहुरव्ययम् |
+छन्दांसि यस्य पर्णानि यस्तं वेद स वेदवित् || 1||
+
+Verse-1
+śhrī-bhagavān uvācha
+ūrdhva-mūlam adhaḥ-śhākham aśhvatthaṁ prāhur avyayam
+chhandānsi yasya parṇāni yas taṁ veda sa veda-vit
+Verse-1
+śhrī-bhagavān uvācha—the Supreme Divine Personality said; ūrdhva-mūlam—with roots above; adhaḥ—downward; śhākham—branches; aśhvattham—the sacred fig tree; prāhuḥ—they speak; avyayam—eternal; chhandānsi—Vedic mantras; yasya—of which; parṇāni—leaves; yaḥ—who; tam—that; veda—knows; saḥ—he; veda-vit—the knower of the Vedas
+Verse-1
+BG 15.1: The Supreme Divine Personality said: They speak of an eternal aśhvatth tree with its roots above and branches below. Its leaves are the Vedic hymns, and one who knows the secret of this tree is the knower of the Vedas.
+Verse-1
+
+Verse-2
+अधश्चोर्ध्वं प्रसृतास्तस्य शाखा
+गुणप्रवृद्धा विषयप्रवाला: |
+अधश्च मूलान्यनुसन्ततानि
+कर्मानुबन्धीनि मनुष्यलोके || 2||
+
+Verse-2
+adhaśh chordhvaṁ prasṛitās tasya śhākhā
+guṇa-pravṛiddhā viṣhaya-pravālāḥ
+adhaśh cha mūlāny anusantatāni
+karmānubandhīni manuṣhya-loke
+Verse-2
+adhaḥ—downward; cha—and; ūrdhvam—upward; prasṛitāḥ—extended; tasya—its; śhākhāḥ—branches; guṇa—modes of material nature; pravṛiddhāḥ—nourished; viṣhaya—objects of the senses; pravālāḥ—buds; adhaḥ—downward; cha—and; mūlāni—roots; anusantatāni—keep growing; karma—actions; anubandhīni—bound; manuṣhya-loke—in the world of humans
+Verse-2
+BG 15.2: The branches of the tree extend upward and downward, nourished by the three guṇas, with the objects of the senses as tender buds. The roots of the tree hang downward, causing the flow of karma in the human form. Below, its roots branch out causing (karmic) actions in the world of humans.
+Verse-2
+
+
+Verse-3
+न रूपमस्येह तथोपलभ्यते
+नान्तो न चादिर्न च सम्प्रतिष्ठा |
+अश्वत्थमेनं सुविरूढमूल
+मसङ्गशस्त्रेण दृढेन छित्वा || 3||
+
+Verse-3
+na rūpam asyeha tathopalabhyate
+nānto na chādir na cha sampratiṣhṭhā
+aśhvattham enaṁ su-virūḍha-mūlam
+asaṅga-śhastreṇa dṛiḍhena chhittvā
+
+Verse-3
+na—not; rūpam—form; asya—of this; iha—in this world; tathā—as such; upalabhyate—is perceived; na—neither; antaḥ—end; na—nor; cha—also; ādiḥ—beginning; na—never; cha—also; sampratiṣhṭhā—the basis; aśhvattham—sacred fig tree; enam—this; su-virūḍha-mūlam—deep-rooted; asaṅga-śhastreṇa—by the axe of detachment; dṛiḍhena—strong; chhittvā—having cut down;
+Verse-3
+BG 15.3-4: The real form of this tree is not perceived in this world, neither its beginning nor end, nor its continued existence. But this deep-rooted aśhvatth tree must be cut down with a strong axe of detachment. Then one must search out the base of the tree, which is the Supreme Lord, from whom streamed forth the activity of the universe a long time ago. Upon taking refuge in him, one will not return to this world again.
+Verse-3
+**************translation to be checked
+
+
+Verse-4
+तत: पदं तत्परिमार्गितव्यं
+यस्मिन्गता न निवर्तन्ति भूय: |
+तमेव चाद्यं पुरुषं प्रपद्ये
+यत: प्रवृत्ति: प्रसृता पुराणी || 4||
+
+Verse-4
+tataḥ padaṁ tat parimārgitavyaṁ
+yasmin gatā na nivartanti bhūyaḥ
+tam eva chādyaṁ puruṣhaṁ prapadye
+yataḥ pravṛittiḥ prasṛitā purāṇī
+Verse-4
+tataḥ—then; padam—place; tat—that; parimārgitavyam—one must search out; yasmin—where; gatāḥ—having gone; na—not; nivartanti—return; bhūyaḥ—again; tam—to him; eva—certainly; cha—and; ādyam—original; puruṣham—the Supreme Lord; prapadye—take refuge; yataḥ—whence; pravṛittiḥ—the activity; prasṛitā—streamed forth; purāṇi—very old
+Verse-4 
+BG 15.3-4: The real form of this tree is not perceived in this world, neither its beginning nor end, nor its continued existence. But this deep-rooted aśhvatth tree must be cut down with a strong axe of detachment. Then one must search out the base of the tree, which is the Supreme Lord, from whom streamed forth the activity of the universe a long time ago. Upon taking refuge in him, one will not return to this world again.
+Verse-4
+**************translation to be checked
+
+
+Verse-5 
+ निर्मानमोहा जितसङ्गदोषा
+अध्यात्मनित्या विनिवृत्तकामा: |
+द्वन्द्वैर्विमुक्ता: सुखदु:खसंज्ञै
+र्गच्छन्त्यमूढा: पदमव्ययं तत् || 5||
+
+Verse-5
+nirmāna-mohā jita-saṅga-doṣhā
+adhyātma-nityā vinivṛitta-kāmāḥ
+dvandvair vimuktāḥ sukha-duḥkha-sanjñair
+gachchhanty amūḍhāḥ padam avyayaṁ tat
+Verse-5
+niḥ—free from; māna—vanity; mohāḥ—delusion; jita—having overcome; saṅga—attachment; doṣhāḥ—evils; adhyātma-nityāḥ—dwelling constantly in the self and God; vinivṛitta—freed from; kāmāḥ—desire to enjoy senses; dvandvaiḥ—from the dualities; vimuktāḥ—liberated; sukha-duḥkha—pleasure and pain; saṁjñaiḥ—known as; gachchhanti—attain; amūḍhāḥ—unbewildered; padam—abode; avyayam—eternal; tat—that
+Verse-5
+BG 15.5: Those who are free from vanity and delusion, who have overcome the evil of attachment, who dwell constantly in the self and God, who are freed from the desire to enjoy the senses, and are beyond the dualities of pleasure and pain, such liberated personalities attain my eternal abode.
+Verse-5
+
+
+Verse-6
+ न तद्भासयते सूर्यो न शशाङ्को न पावक: |
+यद्गत्वा न निवर्तन्ते तद्धाम परमं मम || 6||
+
+Verse-6
+na tad bhāsayate sūryo na śhaśhāṅko na pāvakaḥ
+yad gatvā na nivartante tad dhāma paramaṁ mama
+
+Verse-6
+na—neither; tat—that; bhāsayate—illumine; sūryaḥ—the sun; na—nor; śhaśhāṅkaḥ—the moon; na—nor; pāvakaḥ—fire; yat—where; gatvā—having gone; na—never; nivartante—they return; tat—that; dhāma—abode; paramam—supreme; mama—mine
+Verse-6
+BG 15.6: Neither the sun nor the moon, nor fire can illumine that supreme abode of mine. Having gone there, one does not return to this material world again.
+Verse-6
+
+
+Verse-7
+ममैवांशो जीवलोके जीवभूत: सनातन: |
+मन:षष्ठानीन्द्रियाणि प्रकृतिस्थानि कर्षति || 7||
+
+Verse-7
+mamaivānśho jīva-loke jīva-bhūtaḥ sanātanaḥ
+manaḥ-ṣhaṣhṭhānīndriyāṇi prakṛiti-sthāni karṣhati
+Verse-7
+mama—my; eva—only; anśhaḥ—fragmental part; jīva-loke—in the material world; jīva-bhūtaḥ—the embodied souls; sanātanaḥ—eternal; manaḥ—with the mind; ṣhaṣhṭhāni—the six; indriyāṇi—senses; prakṛiti-sthāni—bound by material nature; karṣhati—struggling
+Verse-7
+BG 15.7: The embodied souls in this material world are my eternal fragmental parts. But bound by material nature, they are struggling with the six senses including the mind.
+Verse-7
+
+
+Verse-8
+शरीरं यदवाप्नोति यच्चाप्युत्क्रामतीश्वर: |
+गृहीत्वैतानि संयाति वायुर्गन्धानिवाशयात् || 8||
+
+Verse-8
+śharīraṁ yad avāpnoti yach chāpy utkrāmatīśhvaraḥ
+gṛihītvaitāni sanyāti vāyur gandhān ivāśhayāt
+Verse-8
+śharīram—the body; yat—as; avāpnoti—carries; yat—as; cha api—also; utkrāmati—leaves; īśhvaraḥ—the Lord of the material body, the embodied soul; gṛihītvā—taking; etāni—these; sanyāti—goes away; vāyuḥ—the air; gandhān—fragrance; iva—like; āśhayāt—from seats
+Verse-8
+BG 15.8: As the air carries fragrance from place to place, so does the embodied soul carry the mind and senses with it, when it leaves an old body and enters a new one.
+Verse-8
+
+
+
+
+Verse-9
+श्रोत्रं चक्षु: स्पर्शनं च रसनं घ्राणमेव च |
+अधिष्ठाय मनश्चायं विषयानुपसेवते || 9||
+
+Verse-9
+śhrotraṁ chakṣhuḥ sparśhanaṁ cha rasanaṁ ghrāṇam eva cha
+adhiṣhṭhāya manaśh chāyaṁ viṣhayān upasevate
+Verse-9
+śhrotram—ears; chakṣhuḥ—eyes; sparśhanam—the sense of touch; cha—and; rasanam—tongue; ghrāṇam—nose; eva—also; cha—and; adhiṣhṭhāya—grouped around; manaḥ—mind; cha—also; ayam—they; viṣhayān—sense objects; upasevate—savors
+Verse-9
+BG 15.9: Using the sense perceptions of the ears, eyes, skin, tongue, and nose, which are grouped around the mind, the embodied soul savors the objects of the senses.
+Verse-9
+
+
+Verse-10
+उत्क्रामन्तं स्थितं वापि भुञ्जानं वा गुणान्वितम् |
+विमूढा नानुपश्यन्ति पश्यन्ति ज्ञानचक्षुष: || 10||
+
+Verse-10
+utkrāmantaṁ sthitaṁ vāpi bhuñjānaṁ vā guṇānvitam
+vimūḍhā nānupaśhyanti paśhyanti jñāna-chakṣhuṣhaḥ
+Verse-10
+utkrāmantam—departing; sthitam—residing; vā api—or even; bhuñjānam—enjoys; vā—or; guṇa-anvitam—under the spell of the modes of material nature; vimūḍhāḥ—the ignorant; na—not; anupaśhyanti—percieve; paśhyanti—behold; jñāna-chakṣhuṣhaḥ—those who possess the eyes of knowledge
+Verse-10
+BG 15.10: The ignorant do not perceive the soul as it resides in the body, and as it enjoys sense objects; nor do they perceive it when it departs. But those who possess the eyes of knowledge can behold it.
+Verse-10
+
+
+Verse-11
+यतन्तो योगिनश्चैनं पश्यन्त्यात्मन्यवस्थितम् |
+यतन्तोऽप्यकृतात्मानो नैनं पश्यन्त्यचेतस: || 11||
+
+Verse-11
+yatanto yoginaśh chainaṁ paśhyanty ātmany avasthitam
+yatanto ‘py akṛitātmāno nainaṁ paśhyanty achetasaḥ
+Verse-11
+yatantaḥ—striving; yoginaḥ—yogis; cha—too; enam—this (the soul); paśhyanti—see; ātmani—in the body; avasthitam—enshrined; yatantaḥ—strive; api—even though; akṛita-ātmānaḥ—those whose minds are not purified; na—not; enam—this; paśhyanti—cognize; achetasaḥ—unaware
+Verse-11
+BG 15.11: Striving yogis too are able to realize the soul enshrined in the body. However, those whose minds are not purified cannot cognize it, even though they strive to do so.
+Verse-11
+
+
+Verse-12
+यदादित्यगतं तेजो जगद्भासयतेऽखिलम् |
+यच्चन्द्रमसि यच्चाग्नौ तत्तेजो विद्धि मामकम् || 12||
+Verse-12
+yad āditya-gataṁ tejo jagad bhāsayate ’khilam
+yach chandramasi yach chāgnau tat tejo viddhi māmakam
+Verse-12
+yat—which; āditya-gatam—in the sun; tejaḥ—brilliance; jagat—solar system; bhāsayate—illuminates; akhilam—entire; yat—which; chandramasi—in the moon; yat—which; cha—also; agnau—in the fire; tat—that; tejaḥ—brightness; viddhi—know; māmakam—mine
+Verse-12
+BG 15.12: Know that I am like the brilliance of the sun that illuminates the entire solar system. The radiance of the moon and the brightness of the fire also come from me.
+Verse-12
+
+
+Verse-13
+गामाविश्य च भूतानि धारयाम्यहमोजसा |
+पुष्णामि चौषधी: सर्वा: सोमो भूत्वा रसात्मक: || 13||
+
+Verse-13
+gām āviśhya cha bhūtāni dhārayāmy aham ojasā
+puṣhṇāmi chauṣhadhīḥ sarvāḥ somo bhūtvā rasātmakaḥ
+
+Verse-13
+gām—earth; āviśhya—permeating; cha—and; bhūtāni—living beings; dhārayāmi—sustain; aham—I; ojasā—energy; puṣhṇāmi—nourish; cha—and; auṣhadhīḥ—plants; sarvāḥ—all; somaḥ—the moon; bhūtvā—becoming; rasa-ātmakaḥ—supplying the juice of life
+Verse-13
+BG 15.13: Permeating the earth, I nourish all living beings with my energy. Becoming the moon, I nourish all plants with the juice of life.
+Verse-13
+
+
+Verse-14
+अहं वैश्वानरो भूत्वा प्राणिनां देहमाश्रित: |
+प्राणापानसमायुक्त: पचाम्यन्नं चतुर्विधम् || 14||
+
+Verse-14
+ahaṁ vaiśhvānaro bhūtvā prāṇināṁ deham āśhritaḥ
+prāṇāpāna-samāyuktaḥ pachāmy annaṁ chatur-vidham
+
+Verse-14
+aham—I; vaiśhvānaraḥ—fire of digestion; bhūtvā—becoming; prāṇinām—of all living beings; deham—the body; āśhritaḥ—situated; prāṇa-apāna—outgoing and incoming breath; samāyuktaḥ—keeping in balance; pachāmi—I digest; annam—foods; chatuḥ-vidham—the four kinds
+Verse-14
+BG 15.14: It is I who take the form of the fire of digestion in the stomachs of all living beings, and combine with the incoming and outgoing breaths, to digest and assimilate the four kinds of foods.
+Verse-14
+
+
+Verse-15
+सर्वस्य चाहं हृदि सन्निविष्टो
+मत्त: स्मृतिर्ज्ञानमपोहनं च |
+वेदैश्च सर्वैरहमेव वेद्यो
+वेदान्तकृद्वेदविदेव चाहम् || 15||
+
+Verse-15
+sarvasya chāhaṁ hṛidi sanniviṣhṭo
+mattaḥ smṛitir jñānam apohanaṁ cha
+vedaiśh cha sarvair aham eva vedyo
+vedānta-kṛid veda-vid eva chāham
+
+Verse-15
+sarvasya—of all living beings; cha—and; aham—I; hṛidi—in the hearts; sanniviṣhṭaḥ—seated; mattaḥ—from me; smṛitiḥ—memory; jñānam—knowledge; apohanam—forgetfulness; cha—as well as; vedaiḥ—by the Vedas; cha—and; sarvaiḥ—all; aham—I; eva—alone; vedyaḥ—to be known; vedānta-kṛit—the author of the Vedānt; veda-vit—the knower of the meaning of the Vedas; eva—alone; cha—and; aham—I
+Verse-15
+BG 15.15: I am seated in the hearts of all living beings, and from me come memory, knowledge, as well as forgetfulness. I alone am to be known by all the Vedas, am the author of the Vedānt, and the knower of the meaning of the Vedas.
+Verse-15
+
+
+Verse-16
+द्वाविमौ पुरुषौ लोके क्षरश्चाक्षर एव च |
+क्षर: सर्वाणि भूतानि कूटस्थोऽक्षर उच्यते || 16||
+Verse-16
+dvāv imau puruṣhau loke kṣharaśh chākṣhara eva cha
+kṣharaḥ sarvāṇi bhūtāni kūṭa-stho ’kṣhara uchyate
+Verse-16
+dvau—two; imau—these; puruṣhau—beings; loke—in creation; kṣharaḥ—the perishable; cha—and; akṣharaḥ—the imperishable; eva—even; cha—and; kṣharaḥ—the perishable; sarvāṇi—all; bhūtāni—beings; kūṭa-sthaḥ—the liberated; akṣharaḥ—the imperishable; uchyate—is said
+Verse-16
+BG 15.16: There are two kinds of beings in creation, the kṣhar (perishable) and the akṣhar (imperishable). The perishable are all beings in the material realm. The imperishable are the the liberated beings.
+Verse-16
+
+
+Verse-17
+उत्तम: पुरुषस्त्वन्य: परमात्मेत्युदाहृत: |
+यो लोकत्रयमाविश्य बिभर्त्यव्यय ईश्वर: || 17||
+
+Verse-17
+uttamaḥ puruṣhas tv anyaḥ paramātmety udāhṛitaḥ
+yo loka-trayam āviśhya bibharty avyaya īśhvaraḥ
+
+Verse-17
+uttamaḥ—the Supreme; puruṣhaḥ—Divine Personality; tu—but; anyaḥ—besides; parama-ātmā—the Supreme Soul; iti—thus; udāhṛitaḥ—is said; yaḥ—who; loka trayam—the three worlds; āviśhya—enters; bibharti—supports; avyayaḥ—indestructible; īśhvaraḥ—the controller
+Verse-17
+BG 15.17: Besides these, is the Supreme Divine Personality, who is the indestructible Supreme Soul. He enters the three worlds as the unchanging controller and supports all living beings.
+Verse-17
+
+
+Verse-18
+यस्मात्क्षरमतीतोऽहमक्षरादपि चोत्तम: |
+अतोऽस्मि लोके वेदे च प्रथित: पुरुषोत्तम: || 18||
+
+Verse-18
+yasmāt kṣharam atīto ’ham akṣharād api chottamaḥ
+ato ’smi loke vede cha prathitaḥ puruṣhottamaḥ
+
+Verse-18
+yasmāt—hence; kṣharam—to the perishable; atītaḥ—transcendental; aham—I; akṣharāt—to the imperishable; api—even; cha—and; uttamaḥ—transcendental; ataḥ—therefore; asmi—I am; loke—in the world; vede—in the Vedas; cha—and; prathitaḥ—celebrated; puruṣha-uttamaḥ—as the Supreme Divine Personality
+Verse-18
+BG 15.18: I am transcendental to the perishable world of matter, and even to the imperishable soul; hence I am celebrated, both in the Vedas and the Smṛitis, as the Supreme Divine Personality.
+Verse-18
+
+
+Verse-19
+यो मामेवमसम्मूढो जानाति पुरुषोत्तमम् |
+स सर्वविद्भजति मां सर्वभावेन भारत || 19||
+
+Verse-19
+yo mām evam asammūḍho jānāti puruṣhottamam
+sa sarva-vid bhajati māṁ sarva-bhāvena bhārata
+
+Verse-19
+yaḥ—who; mām—me; evam—thus; asammūḍhaḥ—without a doubt; jānāti—know; puruṣha-uttamam—the Supreme Divine Personality; saḥ—they; sarva-vit—those with complete knowledge; bhajati—worship; mām—me; sarva-bhāvena—with one’s whole being; bhārata—Arjun, the son of Bharat
+Verse-19
+BG 15.19: Those who know me without doubt as the Supreme Divine Personality truly have complete knowledge. O Arjun, they worship me with their whole being.
+Verse-19
+
+
+Verse-20
+इति गुह्यतमं शास्त्रमिदमुक्तं मयानघ |
+एतद्बुद्ध्वा बुद्धिमान्स्यात्कृतकृत्यश्च भारत || 20||
+
+Verse-20
+iti guhyatamaṁ śhāstram idam uktaṁ mayānagha
+etad buddhvā buddhimān syāt kṛita-kṛityaśh cha bhārata
+Verse-20
+iti—these; guhya-tamam—most secret; śhāstram—Vedic scriptures; idam—this; uktam—spoken; mayā—by me; anagha—Arjun, the sinless one; etat—this; buddhvā—understanding; buddhi-mān—enlightened; syāt—one becomes; kṛita-kṛityaḥ—who fulfills all that is to be accomplished; cha—and; bhārata—Arjun, the son of Bharat
+Verse-20
+BG 15.20: I have shared this most secret principle of the Vedic scriptures with you, O sinless Arjun. By understanding this, a person becomes enlightened, and fulfills all that is to be accomplished.
+Verse-20
+
+
+</body>
+</html>
